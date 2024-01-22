24de1b660e416e76a0b14e756002966d3b91d057
Csicskó Réka <csicskor22@ADY-NAGYATAD.SULINET.HU>
2024-01-22 13:42:07 +0100

    2

---
+++ beadando.py
@@ -0,0 +1,104 @@
+list=[]
+import random
+print(random.randint(10000,99999))
+#while range
+
+def paros(lista):
+    parosak=[]
+    for i in range(len(lista)):
+        if lista[i]%2==0:
+            parosak.append(lista[i])
+    MennyiParosVan=len(parosak)
+    return MennyiParosVan
+
+def paratlan(lista):
+    paratlanok=[]
+    osszeg=q
+    for i in range(len(lista)):
+        if lista[i]%2==1:
+            paratlanok.append(lista[i])
+    MennyiParatlanVan=len(paratlanok)
+    osszeg+osszeg+i
+    return  MennyiParatlanVan
+
+print("Ennyi páros szám van benne: ",MennyiParosVan)
+print("A páratlan számok összege: ",MennyiParatlanVan)
+
+#felezés
+ElsoFel=[:249]
+MasodikFel=[249:]
+
+ElsoFel=0
+MasodikFel=0
+
+for i in ElsoFel:
+    ElsoOsszeg=Elsoosszeg+i
+for i in MasodikOsszeg:
+    masodikOsszeg=MasodikOsszeg+i
+if ElsoOsszeg>MasodikOsszeg:
+    print("Az ElsoFel a nagyobb")
+else:
+    print("a MasodikFel a nagyobb")
+
+#3mas
+HarmassalKezdodoSzamok=[]
+HarmasKezdoSzam='3'
+for x in randomlist:
+    if str(x).startswith(HarmasKezdoSzam):
+         HarmassalKezdodoSzamok.append(x)
+#1es
+EgyelKezdodoSzamok=[]
+EgyelKezdoSzam='1'
+for x in randomlist:
+    if str(x).startswith(EgyesKezdoSzam):
+         EgyelKezdodoSzamok.append(x)
+
+#2es
+KettovelKezdodoSzamok=[]
+KettovelKezdoSzam='2'
+for x in randomlist:
+    if str(x).startswith(KettovelKezdoSzam):
+         KettovelKezdodoSzamok.append(x)
+
+#4es
+NegyelKezdodoSzamok=[]
+NegyelKezdoSzam='4'
+for x in randomlist:
+    if str(x).startswith(NegyelKezdoSzam):
+         NegyelKezdodoSzamok.append(x)
+
+#5os
+OttelKezdodoSzamok=[]
+OttelKezdoSzam='5'
+for x in randomlist:
+    if str(x).startswith(OttelKezdoSzam):
+         OttelKezdodoSzamok.append(x)
+
+#6os
+HattalKezdodoSzamok=[]
+HattalKezdoSzam='6'
+for x in randomlist:
+    if str(x).startswith(HattalKezdoSzam):
+         HattalKezdodoSzamok.append(x)
+
+#7es
+HettelKezdodoSzamok=[]
+HettelKezdoSzam='7'
+for x in randomlist:
+    if str(x).startswith(HettelKezdoSzam):
+         HettelKezdodoSzamok.append(x)
+
+#8as
+NyolccalKezdodoSzamok=[]
+NyolccalKezdoSzam='8'
+for x in randomlist:
+    if str(x).startswith(NyolccalKezdoSzam):
+         NyolccalKezdodoSzamok.append(x)
+
+#9cel
+KilenccelKezdodoSzamok=[]
+KilenccelKezdoSzam='5'
+for x in randomlist:
+    if str(x).startswith(KilenccelKezdoSzam):
+         KilenccelKezdodoSzamok.append(x)
+
